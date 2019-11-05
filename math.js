106ee7e5cfab07d79d18dfb14ba8fa09c1f84d89
shushengjie <951341470@qq.com>
2019-11-05 17:06:54 +0800

    study

--- math.js
+++ math.js
@@ -1,82 +1,214 @@
-/*
-随机生成一个长度为 10 的整数类型的数组，例如 [2, 10, 3, 4, 5, 11, 10, 11, 20]，将其排列成一个新数组，要求新数组形式如下，例如 [[2, 3, 4, 5], [10, 11], [20]]。
-*/
+// /*
+// 随机生成一个长度为 10 的整数类型的数组，例如 [2, 10, 3, 4, 5, 11, 10, 11, 20]，将其排列成一个新数组，要求新数组形式如下，例如 [[2, 3, 4, 5], [10, 11], [20]]。
+// */
 
-// 生成范围内随机整数
-let randomNum = function(min, max) {
-  if (!max) {
-    max = min;
-    min = 0;
-  }
-  return min + (Math.floor(Math.random() * (max - min + 1)))
-}
-// 传入数组长度，随机最小值和最大值
-let randomArr = function(len,min,max) {
-  if((max-min)<len){ //可生成数的范围小于数组长度
-      return null;
-  }
-  let hash = [];
+// // 生成范围内随机整数
+// let randomNum = function(min, max) {
+//   if (!max) {
+//     max = min;
+//     min = 0;
+//   }
+//   return min + (Math.floor(Math.random() * (max - min + 1)))
+// }
+// // 传入数组长度，随机最小值和最大值
+// let randomArr = function(len,min,max) {
+//   if((max-min)<len){ //可生成数的范围小于数组长度
+//       return null;
+//   }
+//   let hash = [];
 
-  while(hash.length<len){
-    let num = randomNum(min,max);
+//   while(hash.length<len){
+//     let num = randomNum(min,max);
       
-      if(hash.indexOf(num) === -1){
-            hash.push(num);
-      }
+//       if(hash.indexOf(num) === -1){
+//             hash.push(num);
+//       }
+//   }
+//   return hash;
+// }
+// // 将数组按十位数分类
+// let change2newArr = function(numArr) {
+//   let newArr = Array.from(new Set(numArr)).sort((a, b) => a - b);
+//   const map = new Map();
+//   newArr.forEach((v) => {
+//     const key = Math.floor(v / 10);
+//     const group = map.get(key) || [];
+//     group.push(v);
+//     map.set(key, group);
+//   })
+//   return [...map.values()];
+// }
+// let newArr = change2newArr(randomArr(10, 1, 30))
+
+
+//  /*
+//     给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+
+//     你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+
+//     示例:
+
+//     给定 nums = [2, 7, 11, 15], target = 9
+
+//     因为 nums[0] + nums[1] = 2 + 7 = 9
+//     所以返回 [0, 1]
+//   */
+//  let twoSum = function(nums, target) {
+//   const map = {};
+//   let result = [];
+//   nums.forEach((num, index) => {
+//     const targetNum = target - nums[index];
+//     if (targetNum in map) {
+//       result = [map[targetNum], index];
+//     }
+//     map[nums[index]] = index;
+//   })
+//   return result
+// }
+// // twoSum([2, 7, 11, 15], 9)
+
+//   /*
+//   如下：{1:222, 2:123, 5:888}，请把数据处理为如下结构：[222, 123, null, null, 888, null, null, null, null, null, null, null]。
+//   */
+//   let scale = {
+//     1: 222,
+//     2: 123,
+//     5: 888
+//   }
+//   let result = Array.from({length:12}).map((val, index) => {
+//     console.log(index)
+//     return scale[index + 1] || null
+//   });
+//   console.log(result)
+  
+//  /**
+//   *  给定字符串J 代表石头中宝石的类型，和字符串 S代表你拥有的石头。 S 中每个字符代表了一种你拥有的石头的类型，你想知道你拥有的石头中有多少是宝石。
+
+//       J 中的字母不重复，J 和 S中的所有字符都是字母。字母区分大小写，因此"a"和"A"是不同类型的石头。
+
+//       示例 1:
+
+//       输入: J = "aA", S = "aAAbbbb"
+//       输出: 3
+//       示例 2:
+
+//       输入: J = "z", S = "ZZ"
+//       输出: 0
+//       注意:
+
+//       S 和 J 最多含有50个字母。
+//        J 中的字符不重复。
+//   */
+
+//   let numJewelsInStones = function(J, S) {
+//       let JArr = J.split('');
+//       let SArr = S.split('');
+//       return SArr.filter(item => JArr.includes(item)).length
+//   };
+
+//   /**
+//    * 给你一个有效的 IPv4 地址 address，返回这个 IP 地址的无效化版本。
+
+//       所谓无效化 IP 地址，其实就是用 "[.]" 代替了每个 "."。
+
+//        
+
+//       示例 1：
+
+//       输入：address = "1.1.1.1"
+//       输出："1[.]1[.]1[.]1"
+//       示例 2：
+
+//       输入：address = "255.100.50.0"
+//       输出："255[.]100[.]50[.]0"
+//    */
+//   var defangIPaddr = function(address) {
+//     return address.replace(/\./g,"[.]")
+//   };
+
+let ary = [1, [2, [3, [4, 5]]], 6];
+let str = JSON.stringify(ary);
+/**
+ * 1、利用es6中的flat
+ */
+// console.log(ary.flat(Infinity));
+
+/**
+ * 2、利用replace+split
+ */
+// ary = str.replace(/(\[|\])/g, '').split(',');
+// console.log(ary)
+
+/**
+ * 3、利用replace+JSON.parse
+ */
+// str = str.replace(/(\[|\])/g, '');
+// str = '[' + str + ']';
+// console.log(str);
+// ary = JSON.parse(str);
+// console.log(ary)
+
+/**
+ * 4、普通递归
+ */
+// let result = [];
+// let fn = function(ary) {
+//   for (let i = 0; i < ary.length; i++) {
+//     let item = ary[i];
+//     if (Array.isArray(item)) {
+//       fn(item)
+//     } else {
+//       result.push(item)
+//     }
+//   }
+// }
+// fn(ary)
+// console.log(result)
+
+/**
+ * 5、利用reduce函数迭代
+ */
+// function flatten(ary) {
+//   return ary.reduce((pre, cur) => {
+//     return pre.concat(Array.isArray(cur) ? flatten(cur) : cur)
+//   }, [])
+// }
+// console.log(flatten(ary))
+
+/**
+ * 6、扩展运算符
+ */
+// while(ary.some(Array.isArray)){
+//   ary = [].concat(...ary)
+// }
+// console.log(ary)
+
+/**
+ * 模拟实现map函数
+ */
+Array.prototype.myMap = function(callback, thisArg) {
+  // 处理数组类型异常
+  if (this === null || this === undefined) {
+    throw new TypeError('cannot read property of null or undefined')
   }
-  return hash;
-}
-// 将数组按十位数分类
-let change2newArr = function(numArr) {
-  let newArr = Array.from(new Set(numArr)).sort((a, b) => a - b);
-  const map = new Map();
-  newArr.forEach((v) => {
-    const key = Math.floor(v / 10);
-    const group = map.get(key) || [];
-    group.push(v);
-    map.set(key, group);
-  })
-  return [...map.values()];
-}
-let newArr = change2newArr(randomArr(10, 1, 30))
-
-
- /*
-    给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
-
-    你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
-
-    示例:
-
-    给定 nums = [2, 7, 11, 15], target = 9
-
-    因为 nums[0] + nums[1] = 2 + 7 = 9
-    所以返回 [0, 1]
-  */
- let twoSum = function(nums, target) {
-  const map = {};
-  let result = [];
-  nums.forEach((num, index) => {
-    const targetNum = target - nums[index];
-    if (targetNum in map) {
-      result = [map[targetNum], index];
+  // 处理回调类型异常
+  if (Object.prototype.toString.call(callback) !== '[object Function]') {
+    throw new TypeError(callback + 'is not a function')
+  }
+  // 先转换为对象
+  let O = Object(this);
+  let T = thisArg;
+  let len = O.length >>> 0;
+  // length >>> 0 实际上是把前面的空位用0填充，这里的作用是保证len为数字且为整数。
+  let A = new Array(len);
+  for (let i = 0; i < len; i++) {
+    if (i in O) {
+      let value = O[i];
+      let mappedValue = callback.call(T, value, i, O);
+      A[i] = mappedValue
     }
-    map[nums[index]] = index;
-  })
-  return result
-}
-// twoSum([2, 7, 11, 15], 9)
-
-  /*
-  如下：{1:222, 2:123, 5:888}，请把数据处理为如下结构：[222, 123, null, null, 888, null, null, null, null, null, null, null]。
-  */
-  let scale = {
-    1: 222,
-    2: 123,
-    5: 888
   }
-  let result = Array.from({length:12}).map((val, index) => {
-    console.log(index)
-    return scale[index + 1] || null
-  });
-  console.log(result)
+  return A;
+}
+let numArr = [1, 2, 3];
+console.log(numArr.myMap(item => item * 2));
--- object.js
+++ object.js
@@ -15,172 +15,194 @@
   // }
 
 
-  /*
-    链式对象定义
-  */
- class LazyManClass {
-  constructor(name) {
-    // this.name = name;
-    this.taskList = [];
-    this.init(name);
-  }
-  init(name) {
-    this.printLog('name', name);
-    setTimeout(() => {
-      this.next();
-    }, 0)
-  }
-  printLog(type, data) {
-    const LOG_MAP = {
-      'name': `HI, I am ${data}`,
-      'eat': `I am eating ${data}.`,
-      'sleep': `Waited for ${data} seconds.`,
-      'error': `Got something wrrong: ${data}.`
-    }
-    console.log(LOG_MAP[type]);
-  }
-  delayPromise(t) {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        resolve();
-      }, t * 1000);
-    })
-  }
-  createTask(type, data) {
-    return async() => {
-      if (type === 'sleep') {
-        try{
-          await this.delayPromise(data);
-        } catch(e) {
-          this.printLog('error', e)
-        }
-      }
-      this.printLog(type, data);
-      this.next();
-    }
-  }
-  addTask(type, data, isFirst = false) {
-    if (isFirst) {
-      this.taskList.unshift(this.createTask(type, data));
-    } else {
-      this.taskList.push(this.createTask(type, data));
-    }
-  }
-  eat(str) {
-      this.addTask('eat', str);
-      return this;
-  }
-  sleep(t) {
-      this.addTask('sleep', t);
-      return this;
-  }
-  sleepFirst(t) {
-      this.addTask('sleep', t, true);
-      return this;
-  }
-  next() {
-    if (this.taskList.length > 0) {
-      let task = this.taskList.shift();
-      task && task();
-    }
-  }
-}
-function LazyMan(name) {
-  return new LazyManClass(name);
-};
-// LazyMan('Tony').sleep(2).eat('lunch');
-// LazyMan('Tony').eat('lunch').sleep(2).eat('dinner');
-// LazyMan('Tony').eat('lunch').eat('dinner').sleepFirst(2).sleep(3).eat('junk food');
+//   /*
+//     链式对象定义
+//   */
+//  class LazyManClass {
+//   constructor(name) {
+//     // this.name = name;
+//     this.taskList = [];
+//     this.init(name);
+//   }
+//   init(name) {
+//     this.printLog('name', name);
+//     setTimeout(() => {
+//       this.next();
+//     }, 0)
+//   }
+//   printLog(type, data) {
+//     const LOG_MAP = {
+//       'name': `HI, I am ${data}`,
+//       'eat': `I am eating ${data}.`,
+//       'sleep': `Waited for ${data} seconds.`,
+//       'error': `Got something wrrong: ${data}.`
+//     }
+//     console.log(LOG_MAP[type]);
+//   }
+//   delayPromise(t) {
+//     return new Promise(resolve => {
+//       setTimeout(() => {
+//         resolve();
+//       }, t * 1000);
+//     })
+//   }
+//   createTask(type, data) {
+//     return async() => {
+//       if (type === 'sleep') {
+//         try{
+//           await this.delayPromise(data);
+//         } catch(e) {
+//           this.printLog('error', e)
+//         }
+//       }
+//       this.printLog(type, data);
+//       this.next();
+//     }
+//   }
+//   addTask(type, data, isFirst = false) {
+//     if (isFirst) {
+//       this.taskList.unshift(this.createTask(type, data));
+//     } else {
+//       this.taskList.push(this.createTask(type, data));
+//     }
+//   }
+//   eat(str) {
+//       this.addTask('eat', str);
+//       return this;
+//   }
+//   sleep(t) {
+//       this.addTask('sleep', t);
+//       return this;
+//   }
+//   sleepFirst(t) {
+//       this.addTask('sleep', t, true);
+//       return this;
+//   }
+//   next() {
+//     if (this.taskList.length > 0) {
+//       let task = this.taskList.shift();
+//       task && task();
+//     }
+//   }
+// }
+// function LazyMan(name) {
+//   return new LazyManClass(name);
+// };
+// // LazyMan('Tony').sleep(2).eat('lunch');
+// // LazyMan('Tony').eat('lunch').sleep(2).eat('dinner');
+// // LazyMan('Tony').eat('lunch').eat('dinner').sleepFirst(2).sleep(3).eat('junk food');
 
-/**
- * 
- * 冒泡排序及改进
- */
- function bubbleSort(arr) {
-    for (let i = 0; i < arr.length; i++) {
-      for (let j = 0; j < arr.length - i - 1; j ++) {
-        if (arr[j] > arr[j + 1]) {
-          const temp = arr[j];
-          arr[j] = arr[j + 1];
-          arr[j + 1] = temp;
-        }
-      }
-    }
-    return arr
-  }
-  console.time('maopao');
-  bubbleSort(randomArr)
-  console.timeEnd('maopao')
+// /**
+//  * 
+//  * 冒泡排序及改进
+//  */
+//  function bubbleSort(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//       for (let j = 0; j < arr.length - i - 1; j ++) {
+//         if (arr[j] > arr[j + 1]) {
+//           const temp = arr[j];
+//           arr[j] = arr[j + 1];
+//           arr[j + 1] = temp;
+//         }
+//       }
+//     }
+//     return arr
+//   }
+//   console.time('maopao');
+//   bubbleSort(randomArr)
+//   console.timeEnd('maopao')
 
 
-  function bubbleSort1(arr) {
-    let i = arr.length - 1;
-    while(i > 0) {
-      let pos = 0;
-      for (let j = 0; j < i; j++) {
-        if (arr[j] > arr[j + 1]) {
-          pos = j;
-          const temp = arr[j];
-          arr[j] = arr[j + 1];
-          arr[j + 1] = temp
-        }
-      }
-      i = pos;
-    }
-    return arr;
-  }
-  console.time('maopao');
-  bubbleSort1(randomArr)
-  console.timeEnd('maopao')
+//   function bubbleSort1(arr) {
+//     let i = arr.length - 1;
+//     while(i > 0) {
+//       let pos = 0;
+//       for (let j = 0; j < i; j++) {
+//         if (arr[j] > arr[j + 1]) {
+//           pos = j;
+//           const temp = arr[j];
+//           arr[j] = arr[j + 1];
+//           arr[j + 1] = temp
+//         }
+//       }
+//       i = pos;
+//     }
+//     return arr;
+//   }
+//   console.time('maopao');
+//   bubbleSort1(randomArr)
+//   console.timeEnd('maopao')
 
+
+//   /**
+//    * 
+//    * 
+//    * 实现 (5).add(3).minus(2) 功能。
+//    */
+//   Number.prototype.add = function(number) {
+//     return this.valueOf() + number
+//   }
+//   Number.prototype.minus = function(number) {
+//     return this.valueOf() - number
+//   }
+//   console.log((5).add(1).minus(2))
+
+//   /**
+//    * 
+//    * sleep函数
+//    */
+
+
+//   const sleep = (time) => {
+//     return new Promise(resolve => {
+//       setTimeout(resolve, time)
+//     })
+//   }
+//   async function run() {
+//     console.log('ready')
+//     await sleep(1000)
+//     console.log('run')
+//   }
+//   run();
+
+//   const sleep = time => {
+//   return new Promise(resolve => setTimeout(resolve,time))
+//   }
+//   sleep(1000).then(()=>{
+//     console.log(1)
+//   })
+
+//   /**
+//    * 
+//    * 
+//    * 已知如下数组：
+//     var arr = [ [1, 2, 2], [3, 4, 5, 5], [6, 7, 8, 9, [11, 12, [12, 13, [14] ] ] ], 10];
+//     编写一个程序将数组扁平化去并除其中重复部分数据，最终得到一个升序且不重复的数组
+
+//    */
+
+//   let arr1 = [ [1, 2, 2], [3, 4, 5, 5], [6, 7, 8, 9, [11, 12, [12, 13, [14] ] ] ], 10];
+//   let newArr1 = new Set(arr1.flat(Infinity).sort((a, b) => a - b));
+//   console.log(newArr1)
 
   /**
-   * 
-   * 
-   * 实现 (5).add(3).minus(2) 功能。
+   * 手动实现instanceof
    */
-  Number.prototype.add = function(number) {
-    return this.valueOf() + number
+  function myInstanceof(left, right) {
+    if (typeof left !== 'object' || left === null) {
+      return false;
+    }
+    let proto = Object.getPrototypeOf(left);
+    while (true) {
+      if (proto === null) {
+        return false;
+      }
+      if (proto === right.prototype) {
+        return true
+      }
+      proto = Object.getPrototypeOf(proto);
+    }
   }
-  Number.prototype.minus = function(number) {
-    return this.valueOf() - number
-  }
-  console.log((5).add(1).minus(2))
+  console.log(myInstanceof("111", String)); //false
+  console.log(myInstanceof(new String("111"), String));//true
 
-  /**
-   * 
-   * sleep函数
-   */
-
-
-  const sleep = (time) => {
-    return new Promise(resolve => {
-      setTimeout(resolve, time)
-    })
-  }
-  async function run() {
-    console.log('ready')
-    await sleep(1000)
-    console.log('run')
-  }
-  run();
-
-  const sleep = time => {
-  return new Promise(resolve => setTimeout(resolve,time))
-  }
-  sleep(1000).then(()=>{
-    console.log(1)
-  })
-
-  /**
-   * 
-   * 
-   * 已知如下数组：
-    var arr = [ [1, 2, 2], [3, 4, 5, 5], [6, 7, 8, 9, [11, 12, [12, 13, [14] ] ] ], 10];
-    编写一个程序将数组扁平化去并除其中重复部分数据，最终得到一个升序且不重复的数组
-
-   */
-
-  let arr1 = [ [1, 2, 2], [3, 4, 5, 5], [6, 7, 8, 9, [11, 12, [12, 13, [14] ] ] ], 10];
-  let newArr1 = new Set(arr1.flat(Infinity).sort((a, b) => a - b));
-  console.log(newArr1)
